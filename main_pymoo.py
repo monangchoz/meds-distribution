71b9d7d46a3dc349983f11db3129870c8100fdd2
gemilang <git.gemilang.santiyuda@gmail.com>
2025-04-02 08:48:54 +0800

    otw mh

--- main_pymoo.py
+++ main_pymoo.py
@@ -1,14 +1,18 @@
 import pathlib
 
-from pymoo_interface.hvrp3l_opt import HVRP3L_OPT
+from pymoo.algorithms.soo.nonconvex.de import DE
+from pymoo.optimize import minimize
 
 from problem.hvrp3l import HVRP3L
+from pymoo_interface.hvrp3l_opt import HVRP3L_OPT
 
 def run():
     filename = "JK2_nc_30_ni__1363_nv_20_0.json"
     instance_filepath = pathlib.Path()/"instances"/filename
     problem = HVRP3L.read_from_json(instance_filepath)
-
+    problem_intf = HVRP3L_OPT(problem)
+    algo = DE()
+    res = minimize(problem_intf, algo)
 
 if __name__ == "__main__":
     run()
--- pymoo_interface/hvrp3l_opt.py
+++ pymoo_interface/hvrp3l_opt.py
@@ -1,30 +1,49 @@
+import math
 
 import numpy as np
 from pymoo.core.problem import ElementwiseProblem
 
+from ep_heuristic.random_slpack import random_slpack
 from problem.hvrp3l import HVRP3L
 from problem.solution import Solution
 
 
+def insert_items(solution: Solution, route: np.ndarray, vehicle_idx: int)->bool:
+    
+    
+    item_dims = solution.item_dims
+    
+
 class HVRP3L_OPT(ElementwiseProblem):
-    def __init__(self, 
+    def __init__(self,
                  hvrp3l_instance: HVRP3L,
-                 elementwise=True, 
                  **kwargs):
-        super().__init__(elementwise, **kwargs)
+        super().__init__(elementwise=True, **kwargs)
         self.hvrp3l_instance = hvrp3l_instance
-        self.num_dims = 2*hvrp3l_instance.num_customers 
+        self.n_var = 2*hvrp3l_instance.num_customers 
         # first num_customers dims are for customer priority
         # the next num_customers dims are for vehicle assignment
-        self.xl = np.zeros([self.num_dims, ], dtype=float)
-        self.xu = np.ones([self.num_dims, ], dtype=float)
+        self.xl = np.zeros([self.n_var, ], dtype=float)
+        self.xu = np.ones([self.n_var, ], dtype=float)
         
     # this is the decoding method
     def _evaluate(self, x, out, *args, **kwargs):
         solution: Solution = Solution(self.hvrp3l_instance)
         # try to map first into vehicle
         # if not feasible?
+        
         for i in range(self.hvrp3l_instance.num_customers, 2*self.hvrp3l_instance.num_customers):
-            print(i)
-        exit()
+            ci = i-self.hvrp3l_instance.num_customers
+            vi = math.floor(x[i]*self.hvrp3l_instance.num_vehicles)
+            solution.cust_vhc_assignment_map[ci] = vi
+        for vi in range(self.hvrp3l_instance.num_vehicles):
+            vi_cust_idx = np.nonzero(solution.cust_vhc_assignment_map==vi)[0]
+            if len(vi_cust_idx) == 0:
+                continue
+            cust_priorities = x[vi_cust_idx]
+            sorted_idx = np.argsort(cust_priorities)
+            vi_cust_idx = vi_cust_idx[sorted_idx]
+        
+        # try inserting, if not feasible, cancel this vehicle assignment, and 
+        # then we repair later.
             
